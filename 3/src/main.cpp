61a2d2fa6720509e2989d7f9a487ded70f630a13
Kaporos <theo@daron.be>
2023-12-04 08:08:19 +0100

    day 3 part 2

--- 3/src/main.cpp
+++ 3/src/main.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <iostream>
 #include <regex>
+#include <unordered_map>
+#include <vector>
 
 
 
@@ -35,9 +37,6 @@ int part1(std::string content) {
       {
         total += stoi(currentNumber);
       }
-      /*else if (currentNumber.size() > 0) {
-        std::cout << currentNumber << "is not ok.." << std::endl;
-      }*/
       currentNumber = "";
       isOkay = false;
       continue; 
@@ -61,16 +60,69 @@ int part1(std::string content) {
 }
 
 int part2(std::string content) {
-  return 0;
-  //Still to-do
+  std::string currentNumber;
+  int lineLength = content.find("\n");
+  std::cout << lineLength << std::endl;
+  int size = content.size();
+  
+  std::unordered_map<int, std::vector<int>> registry;
+  for (int i = 0; i < size; i++) {
+    registry[i] = std::vector<int>();
+  }
+
+  std::vector<int> stars;
+  for (int i = 0; i < size; i++)
+  {
+    char curr = content[i];
+    int value = ctoi(curr);
+    if (value < 0) {
+      if (currentNumber.size() > 0){
+        for (int j = 0; j < stars.size(); j ++) {
+          registry[stars[j]].push_back(stoi(currentNumber));
+        }
+      }
+      stars.clear();
+      currentNumber = "";
+      continue; 
+    }    
+    currentNumber += curr;
+    int toCheck[8] = {i-lineLength, i-lineLength-2, i-lineLength- 1, i+lineLength, i+lineLength+1, i+lineLength+2, i-1, i+1};
+    for (int j = 0; j < 8; j++) {
+      int elem = toCheck[j];
+      if (elem < 0 || elem > size)
+      {
+        continue;
+      }
+      if (content[elem] == '*')
+      {
+        bool already = false;
+        for (int j = 0; j < stars.size(); j++)
+          already = already || (stars[j] == elem);
+        if (!already)
+          stars.push_back(elem);
+      }
+    }
+  }
+
+  int total = 0;
+  for (int i = 0; i < size; i ++) {
+    if (registry[i].size() == 2) {
+      total += (registry[i][0] * registry[i][1]);
+    }
+  }
+
+  
+  
+  return total;
 }
 int main() {
   int total = part1(slurp("data-full.txt"));
   std::cout << "Total part 1: " << total << std::endl;;
-  int total2 = part2(slurp("data.txt"));
+  int total2 = part2(slurp("data-full.txt"));
   std::cout << "Total part 2: " << total2 << std::endl;;
   
   return 0;
 }
 
 
+ 
