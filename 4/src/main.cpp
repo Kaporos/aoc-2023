72b3bdf8b46c46fe1d45329d36cf5a0ee50e3197
Kaporos <theo@daron.be>
2023-12-04 09:59:49 +0100

    cleaning a little bit day 4

--- 4/src/main.cpp
+++ 4/src/main.cpp
@@ -13,55 +13,8 @@ int ctoi(char carac) {
   return -1;
 }
 
-int part1(std::string cardLine) {
-  cardLine += " ";
-  int size = cardLine.size();
-  int id = 0;
-  int count = 0;
-  std::vector<int> possessions;
-  bool inWinning = false;
-  std::string currentNumber = "";
-  for (int i = 0; i < size; i++)
-  {
-    inWinning = inWinning || cardLine[i] == '|';
-    int value = ctoi(cardLine[i]);
-    if (value != -1) {
-      currentNumber += cardLine[i];
-    }
-    if (id == 0 && cardLine[i] == ':') {
-      id = std::stoi(currentNumber);
-      currentNumber = "";
-      continue;
-    }
-    if ((cardLine[i] == ' ' || cardLine[i] == '\n') && currentNumber.size() > 0) {
-      value = std::stoi(currentNumber);
-      currentNumber = "";
-    } else {
-      continue;
-    }
-    if (inWinning) {
-      for (int i = 0; i < possessions.size(); i++) {
-        if (possessions[i] == value) {
-          count += 1;
-        }
-      }
-      continue;
-    }
 
-    possessions.push_back(value);    
-  }
-  if (count == 0) {
-    return count;
-  }
-  int total = 1;
-  count -= 1;
-  for (int i = 0; i < count; i++) {
-    total = total *2;
-  }
-  return total;
-}
-
-int part2(std::string cardLine) {
+int get_count(std::string cardLine) {
   cardLine += " ";
   int size = cardLine.size();
   int id = 0;
@@ -101,6 +54,20 @@ int part2(std::string cardLine) {
   return count;
 }
 
+int part1(std::string cardLine) {
+  int count = get_count(cardLine);
+  if (count == 0) {
+    return count;
+  }
+  int total = 1;
+  count -= 1;
+  for (int i = 0; i < count; i++) {
+    total = total *2;
+  }
+  return total;
+}
+
+
 int main() {
   std::ifstream nameFileout;
   nameFileout.open("data-full.txt");
@@ -126,7 +93,7 @@ int main() {
   }
   for (int i = 0; i < lines; i++) {
     std::getline(nameFileout2, line);
-    int count = part2(line);
+    int count = get_count(line);
     for (int k = i+1; k < i+count+1; k++) {
       count_tables[k] += count_tables[i];
     }
